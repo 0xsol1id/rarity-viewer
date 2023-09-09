4a26d7bfc58cff85f4f588c49132ac18c95c80a2
0xsol1id <0xsol1d@soljunks.io>
2023-09-09 14:43:35 +0200

    main

--- src/views/RarityView/index.tsx
+++ src/views/RarityView/index.tsx
@@ -40,46 +40,71 @@ export const RarityView: FC = ({ }) => {
   const [value, setValue] = useState("")
 
   return (
-    <div className="">
+    <div className="font-pixel">
       <div className="">
-        <div className="mb-1 navbar text-neutral-content rounded flex justify-between bg-base-200">
+        <div className="mb-1 navbar text-neutral-content rounded lg:flex justify-between bg-base-200 hidden">
           <div>
             <button className="">
-              <Link href="/mint" passHref>
-                <a target="_blank"><img src="./Button_mint_1.png" className="h-16" /></a>
-              </Link>
+              <a href="https://degenbags.11pxl.com/mint" target="_blank"><img src="./Button_mint_1.png" className="h-16" /></a>
             </button>
           </div>
-          <div className="flex">
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(junks, true), setPostNumber(50); setNftList(junks[0].nfts.slice(0, postNumber)) }}>SolJunks Gen1</button>
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(faces, true), setPostNumber(50); setNftList(faces[0].nfts.slice(0, postNumber)) }}>Faces of $mb</button>
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(smb, true), setPostNumber(50); setNftList(smb[0].nfts.slice(0, postNumber)) }}>$MB</button>
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(rektiez, true), setPostNumber(50); setNftList(rektiez[0].nfts.slice(0, postNumber)) }}>Lil Rektiez</button>
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(harrddys, true), setPostNumber(50); setNftList(harrddys[0].nfts.slice(0, postNumber)) }}>Harrddy Junks</button>
-            <button className="btn mx-1 mb-1 bg-base-300 rounded border-base-300" onClick={() => { handleChangeCollection(deads, true), setPostNumber(50); setNftList(deads[0].nfts.slice(0, postNumber)) }}>111Deads</button>
+          <div className="flex gap-2">
+            <button className={` ${collection == junks ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(junks, true), setPostNumber(50); setNftList(junks[0].nfts.slice(0, postNumber)) }}>SolJunks Gen1</button>
+            <button className={` ${collection == faces ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(faces, true), setPostNumber(50); setNftList(faces[0].nfts.slice(0, postNumber)) }}>Faces of $mb</button>
+            <button className={` ${collection == smb ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(smb, true), setPostNumber(50); setNftList(smb[0].nfts.slice(0, postNumber)) }}>$MB</button>
+            <button className={` ${collection == rektiez ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(rektiez, true), setPostNumber(50); setNftList(rektiez[0].nfts.slice(0, postNumber)) }}>Lil Rektiez</button>
+            <button className={` ${collection == harrddys ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(harrddys, true), setPostNumber(50); setNftList(harrddys[0].nfts.slice(0, postNumber)) }}>Harrddy Junks</button>
+            <button className={` ${collection == deads ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(deads, true), setPostNumber(50); setNftList(deads[0].nfts.slice(0, postNumber)) }}>111Deads</button>
           </div>
           <div className="mx-2">
             <input
               type="text"
-              placeholder="Enter NFT number"
-              className="font-pixel w-64 input input-bordered mr-2 ml-2 bg-base-200"
+              placeholder="Filter by name"
+              className="font-pixel w-44 input input-bordered mr-2 ml-2 bg-base-200 text-center"
               value={value}
               onChange={(e) => { setValue(e.target.value) }}
             />
           </div>
         </div>
+        <div className="mb-1 text-neutral-content rounded flex lg:hidden justify-between items-center p-2 bg-base-200">
+          <div>
+            <button className="">
+              <a href="https://degenbags.11pxl.com/mint" target="_blank"><img src="./Button_mint_1.png" className="h-16" /></a>
+            </button>
+          </div>
+          <div className="dropdown mx-1 dropdown-hover ">
+            <div tabIndex={0} className="m-1 btn">Collection</div>
+            <ul tabIndex={0} className="p-2 text-md shadow menu dropdown-content bg-base-300 rounded w-40 border border-gray-500">
+              <button className={` ${collection == junks ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(junks, true), setPostNumber(50); setNftList(junks[0].nfts.slice(0, postNumber)) }}>SolJunks Gen1</button>
+              <button className={` ${collection == faces ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(faces, true), setPostNumber(50); setNftList(faces[0].nfts.slice(0, postNumber)) }}>Faces of $mb</button>
+              <button className={` ${collection == smb ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(smb, true), setPostNumber(50); setNftList(smb[0].nfts.slice(0, postNumber)) }}>$MB</button>
+              <button className={` ${collection == rektiez ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(rektiez, true), setPostNumber(50); setNftList(rektiez[0].nfts.slice(0, postNumber)) }}>Lil Rektiez</button>
+              <button className={` ${collection == harrddys ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(harrddys, true), setPostNumber(50); setNftList(harrddys[0].nfts.slice(0, postNumber)) }}>Harrddy Junks</button>
+              <button className={` ${collection == deads ? "bg-primary" : "bg-base-300"} btn`} onClick={() => { handleChangeCollection(deads, true), setPostNumber(50); setNftList(deads[0].nfts.slice(0, postNumber)) }}>111Deads</button>
+            </ul>
+          </div>
+          <div className="mx-2">
+            <input
+              type="text"
+              placeholder="NFT Name"
+              className="font-pixel w-32 input input-bordered mr-2 ml-2 bg-base-200 text-center"
+              value={value}
+              onChange={(e) => { setValue(e.target.value) }}
+            />
+          </div>
+        </div>        
         <div className="overflow-auto h-[76vh] lg:h-[85vh] scrollbar font-pixel p-2" onScroll={handleScroll}>
           <div className="grid grid-cols-3 md:grid-cols-7 gap-2 items-start max-h-[52rem]">
             {value == "" ?
               (nftList.map((nft: any, index: any) => (
                 (nft.Name.includes(value) ? (
-                  <NftCard index={index} nft={nft} />
+                  <NftCard key={index} index={index} nft={nft} />
                 ) : (null)
                 )
               ))) : (
                 (collection[0].nfts.map((nft: any, index: any) => (
                   (nft.Name.includes(value) ? (
-                    <NftCard index={index} nft={nft} />
+                    <NftCard key={index} index={index} nft={nft} />
                   ) : (null)
                   )
                 ))))
@@ -97,12 +122,49 @@ type NftCardProps = {
   nft: any;
 };
 
+const copyAddress = async (val: any) => {
+  await navigator.clipboard.writeText(val);
+}
+
 const NftCard = ({ index, nft }: NftCardProps) => {
   return (
-    <div key={index} className="bg-base-200 p-2">
-      <img src={nft.Image} alt="" />
-      <div>{nft.Name}</div>
-      <div>RANK: {nft.Rank}</div>
+    <div>
+      <label key={index} htmlFor={nft.Name} className="btn btn-ghost modal-button text-white bg-base-200 p-2 h h-full">
+        <div className="mb-2 text-right">👑{nft.Rank}</div>
+        <img src={nft.Image} alt="" />
+        <div className="mt-2">{nft.Name}</div>
+      </label>
+      <input type="checkbox" id={nft.Name} className="modal-toggle" />
+      <div className="modal">
+        <div className="bg-base-200 border-2 rounded-lg p-5 w-[60rem]">
+          <div className="flex justify-between">
+            <div>{nft.Name}</div>
+            <label htmlFor={nft.Name} className="btn">X</label>
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            <img src={nft.Image} alt="" />
+            <div className="bg-gray-700 rounded mt-3 p-2">
+              <div className="flex justify-between">
+                <p>Mint:</p>
+                <div>
+                  <button className="hover:text-red-300 uppercase" onClick={(e: any) => copyAddress(nft.MintHash)}>
+                    {(nft.MintHash).slice(0, 5) + "..." + (nft.MintHash).slice(-5)}
+                  </button>
+                  <a href={`https://solscan.io/address/${nft.MintHash}`} target="_blank"><img src="./solscan_logo.png" alt="tmp" className="h-6 ml-2 hover:border-2 rounded-full" /></a>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <p>Rarity Rank:</p>
+                <p>{nft.Rank}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Rarity Score:</p>
+                <p>{nft.Score.toFixed(2)}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/views/footer.tsx
+++ src/views/footer.tsx
@@ -5,9 +5,9 @@ import { DiscordLogo, TwitterLogo, GithubLogo, GameLogo } from "components"
 export const Footer: React.FC = ({ }) => {
     return (
         <div>
-            <div className="fixed bottom-0 left-0 right-0 bg-base-200 p-2 lg:p-1 lg:flex justify-between hidden">
+            <div className="fixed bottom-0 left-0 right-0 bg-base-200 p-2 lg:p-1 flex justify-between">
             <a href="https://11pxl.com" target="_blank"><img src="./11pxl.png" className="w-20 ml-2 mb-1" /></a>
-                <h1 className="font-trash mr-3 mt-1 text-sm uppercase">v0.2_beta</h1>
+                <h1 className="font-trash mr-3 mt-1 text-sm uppercase">rarity</h1>
                 <div className="flex gap-5 mr-3 mt-1">
                       <a href="https://discord.gg/PRaDjQA9AZ" target="_blank"><DiscordLogo /></a>
                       <a href="https://twitter.com/11pxl" target="_blank"><TwitterLogo /></a>
